ff53189a2e27b5e0d47188930d92aca1763f435d
isaac-co <isaccco_@outlook.com>
2021-04-27 14:07:51 -0500

    Added admin dashboard queries

--- controllers/admin.js
+++ controllers/admin.js
@@ -5,17 +5,40 @@ const Usuario = sequelize.models.usuario;
 
 exports.getDashboard = function(req, res) {
 	if (req.session.loggedin) {
+		var genero = new Array;
+		var interesados = new Array;
+		var ages = new Array;
+		var freq = new Array;
+
 		sequelize.query("CALL getGeneros();")
         .then(registros=>{
-            var lol = [];
-            lol.push(registros[0].Mujeres);
-            lol.push(registros[0].Hombres);
-            console.log(lol);
-			var data = JSON.stringify(registros);
-			res.render('adminDashboard.html', {
-				equisde:lol
-			});
+            genero.push(registros[0].Mujeres);
+            genero.push(registros[0].Hombres);	
         })
+
+		sequelize.query("CALL getInteresados();")
+        .then(registros=>{
+            interesados.push(registros[0].Mujeres);
+            interesados.push(registros[0].Hombres);	
+        })
+
+		sequelize.query("CALL getEdades();")
+        .then(registros=>{
+			console.log();
+			for (let i = 0; i < Object.keys(registros).length; i++) {
+				ages.push(registros[i].age);
+				freq.push(registros[i].Freq);
+			}
+        })
+		
+		.then(registros=>{
+			res.render('adminDashboard.html', {
+				"genero":genero,
+				"interesados":interesados,
+				"freq":freq,
+				"ages":ages,
+			})
+		})
 	} else {
 		res.send('Please login to view this page!');
 	}
